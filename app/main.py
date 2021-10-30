56bc3b12062feabb4bea60564f6aeac10a9743a6
Masayuki Abe <marchin.1989@gmail.com>
2021-10-30 10:15:55 +0900

    initial commit

---
+++ app/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+import datetime
+import mysql.connector
+import json
+from datetime import date, datetime
+
+# date, datetimeの変換関数
+def json_serial(obj):
+    # 日付型の場合には、文字列に変換します
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    # 上記以外はサポート対象外.
+    raise TypeError ("Type %s not serializable" % type(obj))
+
+app = FastAPI()
+
+@app.get("/items")
+def read_item():
+    # dbに接続
+    cnx = mysql.connector.connect(user='mysql', host='127.0.0.1', password="password", database='forum')
+    cursor = cnx.cursor()
+    query = ("SELECT id, message, created_at FROM posts")
+    cursor.execute(query)
+
+    items = list()
+    for (id, message, created_at) in cursor:
+        items.append({"id": id, "message": message, "created_at": created_at})
+
+    return json.dumps({"items": items}, default=json_serial)
+    cursor.close()
+    cnx.close()
+
+    return {"item_id": item_id, "q": q}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
---
+++ bin/setup_env.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+main() {
+  # nginxインストール
+  sudo apt install nginx
+
+  # 起動
+  systemctl status nginx
+
+  # pipをインストール
+  sudo apt install python3-pip
+
+  # uvicornのインストール
+  pip install uvicorn
+
+  # TODO 掲示板アプリソースのインストール
+
+  # PATHを通す
+  echo 'PATH="$HOME/.local/bin/:$PATH"' >> ~/.profile
+  source ~/.profile
+
+  # fast apiインストール
+  pip install fastapi
+
+  # mysqlのインストール
+  sudo apt install mysql-server mysql-client
+
+  # データベースの初期化
+  sudo mysql -u root < init.ddl
+
+  # mysqlとpythonのコネクター
+  pip install mysql-connector-python
+
+  # uvicorn起動
+  uvicorn main:app --reload
+}
+
+main
